0c1deb351a19bc64ab539528e21551ce64445a1f
Vladimir Beev <vladimir.beev89@gmail.com>
2022-06-27 15:43:13 +0300

    Add files via upload

---
+++ C# Learning/Programming Basics with C# - октомври 2021/Deposit Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Deposit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositsum = double.Parse(Console.ReadLine());
+            int period = int.Parse(Console.ReadLine());
+            double lihva = double.Parse(Console.ReadLine())/100;
+            double totalsum = depositsum + period * ((depositsum * lihva) / 12);
+
+            Console.WriteLine(totalsum);
+        }
+    }
+}
