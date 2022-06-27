8d2627616e874e35e0db272c334d1acf303ec467
Vladimir Beev <vladimir.beev89@gmail.com>
2022-06-27 15:40:29 +0300

    Add files via upload

---
+++ C# Learning/Programming Basics with C# - октомври 2021/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Concatenate_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 7;
+            int b = 2;
+            Console.WriteLine(a%b);
+
+        }
+    }
+}
