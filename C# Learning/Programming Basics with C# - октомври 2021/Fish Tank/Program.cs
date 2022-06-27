ba4862eeba3bc4a485c5d6c79d9c8152539af8c8
Vladimir Beev <vladimir.beev89@gmail.com>
2022-06-27 15:43:55 +0300

    Add files via upload

---
+++ C# Learning/Programming Basics with C# - октомври 2021/Fish Tank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fish_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double daljina = double.Parse(Console.ReadLine());
+            double shirina = double.Parse(Console.ReadLine());
+            double visochina = double.Parse(Console.ReadLine());
+            double procent = double.Parse(Console.ReadLine())/100;
+            double sum = daljina * shirina * visochina;
+           
+            double litri = sum / 1000;
+          
+            double pqsak = 1 - procent;
+           
+            double litrisum = litri*pqsak;
+                
+
+            Console.WriteLine(litrisum);
+        }
+    }
+}
