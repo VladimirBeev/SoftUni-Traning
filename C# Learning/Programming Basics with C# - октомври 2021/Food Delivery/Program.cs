a39625254ae847e82a6211cfb0d21d7024c4966b
Vladimir Beev <vladimir.beev89@gmail.com>
2022-06-29 20:14:51 +0300

    Add files via upload

---
+++ C# Learning/Programming Basics with C# - октомври 2021/Food Delivery/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Food_Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pile = double.Parse(Console.ReadLine()) * 10.35;
+            double riba = double.Parse(Console.ReadLine()) * 12.40;
+            double vega = double.Parse(Console.ReadLine()) * 8.15;
+
+            double foodsum = riba + pile + vega;
+            double desert = 0.2 * foodsum;          
+
+            Console.WriteLine(foodsum+desert+2.50);
+        }
+    }
+}
