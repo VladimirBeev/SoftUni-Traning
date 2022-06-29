2afb4be4f9245bd3194bfea6cac95e289fe69e43
Vladimir Beev <vladimir.beev89@gmail.com>
2022-06-29 20:15:19 +0300

    Add files via upload

---
+++ C# Learning/Programming Basics with C# - октомври 2021/Greeting by Name/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Greeting_by_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            char a = '!';
+            Console.WriteLine("Hello, "+ name+a);
+        }
+    }
+}
