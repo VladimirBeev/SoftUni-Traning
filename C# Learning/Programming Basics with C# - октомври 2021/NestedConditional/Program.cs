d9bf032c79655e2a4368736992f45f10352f0f5b
Vladimir Beev <vladimir.beev89@gmail.com>
2022-06-30 20:37:22 +0300

    Add files via upload

---
+++ C# Learning/Programming Basics with C# - октомври 2021/NestedConditional/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NestedConditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int h = 1; h <= 9; h++)
+            {
+                for (int m = 1; m <= 10; m++)
+                {
+                    int sum = h * m;
+                    Console.WriteLine($"{h}*{m}={sum}");
+                }
+            }
+        }
+    }
+}
