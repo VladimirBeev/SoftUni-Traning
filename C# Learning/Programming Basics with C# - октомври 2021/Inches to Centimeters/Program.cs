bc24a364730ccaa4189b86cca8e32543f51159d1
Vladimir Beev <vladimir.beev89@gmail.com>
2022-06-30 20:37:57 +0300

    Add files via upload

---
+++ C# Learning/Programming Basics with C# - октомври 2021/Inches to Centimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Inches_to_Centimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inch = double.Parse(Console.ReadLine());
+            double sm = 2.54 * inch;
+            Console.WriteLine(sm);
+        }
+    }
+}
