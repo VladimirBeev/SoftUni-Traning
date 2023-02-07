bd7df656b43188e07b35c9d45aca7f7bdafabb69
Vladimir Beev <vladimir.beev89@gmail.com>
2023-02-07 12:37:02 +0200

    Add files via upload

---
+++ C# Learning/Programming Basics with C# - октомври 2021/Projects Creation/Projects Creation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Projects_Creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int Project = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The architect {name} will need {Project*3} hours to complete {Project} project/s.");
+
+        }
+    }
+}
