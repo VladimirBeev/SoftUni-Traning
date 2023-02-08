e8307800791e6b337281dea85fe0f374e54a143d
Vladimir Beev <vladimir.beev89@gmail.com>
2023-02-08 17:00:34 +0200

    Add files via upload

---
+++ C# Learning/Programming Basics with C# - октомври 2021/Supplies for School/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Supplies_for_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int penpack = int.Parse(Console.ReadLine());
+            int marckpack = int.Parse(Console.ReadLine());
+            int clean = int.Parse(Console.ReadLine());
+            int discount = int.Parse(Console.ReadLine());
+
+            double sumproduct = penpack * 5.80 + marckpack * 7.20 + (clean * 1.2);
+            double sumdiscount = (discount/100.0);
+            double total = sumproduct - (sumproduct * sumdiscount);
+           
+            Console.WriteLine(total);
+        }
+    }
+}
