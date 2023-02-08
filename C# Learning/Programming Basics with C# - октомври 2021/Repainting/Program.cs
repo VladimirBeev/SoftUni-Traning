bbb8679cd664d6ddf7a9899938423f897dad9f92
Vladimir Beev <vladimir.beev89@gmail.com>
2023-02-08 16:59:42 +0200

    Add files via upload

---
+++ C# Learning/Programming Basics with C# - октомври 2021/Repainting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Repainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pvc = double.Parse(Console.ReadLine());
+            double paint = double.Parse(Console.ReadLine());
+            double thinner = double.Parse(Console.ReadLine());
+            double hours = double.Parse(Console.ReadLine());
+
+            double reserv = (10*paint)/100;
+            double painsum = (paint+reserv)*14.50;
+            double pvcsum = (pvc + 2) * 1.50;
+            double thinnersum = thinner * 5.00;
+
+            double total = pvcsum + painsum + thinnersum + 0.40;
+
+            double master = ((30 * total) / 100) * hours;
+
+            
+            Console.WriteLine(total+master);
+        }
+    }
+}
