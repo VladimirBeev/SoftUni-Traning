0a87defd4a63b415c12c85e7af1afc9c0320843d
Vladimir Beev <vladimir.beev89@gmail.com>
2023-02-08 16:57:42 +0200

    Add files via upload

---
+++ C# Learning/Programming Basics with C# - октомври 2021/Radians to Degrees/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Radians_to_Degrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radian = double.Parse(Console.ReadLine());
+            Console.WriteLine(radian*180 /Math.PI);
+        }
+    }
+}
