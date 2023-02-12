465f3b211d629a1dc51e11f79032b87698ed93f3
Vladimir Beev <vladimir.beev89@gmail.com>
2023-02-12 14:46:30 +0200

    Add files via upload

---
+++ C# Learning/Programming Basics with C# - октомври 2021/Vacation Books List/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Vacation_Books_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pagesinbook = int.Parse(Console.ReadLine());
+            int pagesperhours = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int sumpages = pagesinbook / pagesperhours;
+            int hourssum = sumpages / days;
+            Console.WriteLine(hourssum);
+        }
+    }
+}
