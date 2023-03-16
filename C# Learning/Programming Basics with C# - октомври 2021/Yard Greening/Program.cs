31d84d7a3815f55b16692172c0f6cf7fa494e907
Vladimir Beev <vladimir.beev89@gmail.com>
2023-03-16 21:50:22 +0200

    Add files via upload

---
+++ C# Learning/Programming Basics with C# - октомври 2021/Yard Greening/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Yard_Greening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area = double.Parse(Console.ReadLine());
+            double price = area * 7.61;
+            double discount = 0.18 * price;
+            double total = price - discount;
+
+            Console.WriteLine($"The final price is: {total} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
