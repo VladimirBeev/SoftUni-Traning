f73f441df101efc386fd442e3b9a6cd837c8728f
Vladimir Beev <vladimir.beev89@gmail.com>
2023-03-16 22:47:15 +0200

    Add files via upload

---
+++ C# Learning/C# Advanced/Multidimensional Arrays/2. Squares in Matrix Exercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Squares_in_Matrix_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizeMatrix = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[,] matrix = new int[sizeMatrix[0], sizeMatrix[1]];
+            int counter = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] matrixElements = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row,col] = matrixElements[col];
+                }
+            }
+            for (int row = 0; row < matrix.GetLength(0)-1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1)-1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1])
+                    {
+                        if (matrix[row, col] == matrix[row + 1, col + 1])
+                        {
+                            if (matrix[row, col] == matrix[row + 1, col])
+                            {
+                                counter++;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ C# Learning/C# Advanced/Multidimensional Arrays/3. Maximal Sum Exercise/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._Maximal_Sum_Exercise
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] sizeOfMatrix = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[,] matrix = new int[sizeOfMatrix[0], sizeOfMatrix[1]];
+            int totalSum = 0;
+            int[] firstNums = new int[3];
+            int[] secondNums = new int[3];
+            int[] thirdNums = new int[3];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] elementsArray = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = elementsArray[col];
+                }
+            }
+            for (int row = 0; row < matrix.GetLength(0) - 2; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 2; col++)
+                {
+                    int firstRow = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2];
+                    int secondRow = matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2];
+                    int thirdRow = matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+                    int currentSum = firstRow + secondRow + thirdRow;
+                    if (currentSum > totalSum)
+                    {
+                        totalSum = currentSum;
+                        firstNums = new int[3] { matrix[row, col], matrix[row, col + 1], matrix[row, col + 2] };
+                        secondNums = new int[3] { matrix[row + 1, col] , matrix[row + 1, col + 1] , matrix[row + 1, col + 2]};
+                        thirdNums = new int[3] { matrix[row + 2, col] , matrix[row + 2, col + 1] , matrix[row + 2, col + 2] };
+                        currentSum = 0;
+                    }
+                    currentSum = 0;
+                }
+            }
+            Console.WriteLine($"Sum = {totalSum}");
+            foreach (var num in firstNums)
+                Console.Write($"{num} ");
+            Console.WriteLine();
+            foreach (var num in secondNums)
+                Console.Write($"{num} ");
+            Console.WriteLine();
+            foreach (var num in thirdNums)
+                Console.Write($"{num} ");
+        }
+    }
+}
