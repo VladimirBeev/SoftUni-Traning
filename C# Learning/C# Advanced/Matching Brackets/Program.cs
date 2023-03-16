445a9db3f91ba87a0aec8deca4fad60f03cf70d7
Vladimir Beev <vladimir.beev89@gmail.com>
2023-03-16 22:31:32 +0200

    Add files via upload

---
+++ C# Learning/C# Advanced/Matching Brackets/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string expr = Console.ReadLine();
+            var stack = new Stack<int>();    
+            for (int i = 0; i< expr.Length; i++)
+            {
+                char c = expr[i];
+                if (c == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (c == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int endIndex = i;
+                    string subexpr = expr.Substring(startIndex, endIndex - startIndex+1);
+                    Console.WriteLine(subexpr);
+
+                }
+            }
+        }
+    }
+}
