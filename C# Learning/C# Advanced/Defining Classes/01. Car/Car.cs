f9109d606edb39db9180f593341718acbac2219a
Vladimir Beev <vladimir.beev89@gmail.com>
2023-03-19 21:15:36 +0200

    Add files via upload

---
+++ C# Learning/C# Advanced/Defining Classes/01. Car/Car.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+        public Car(string make, string model, int year)
+            : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+            : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+
+        private string make;
+
+        private string model;
+
+        private int year;
+
+        private double fuelQuantity;
+
+        private double fuelConsumption;
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+        public Engine Engine { get; set; }
+
+        public Tire[] Tires { get; set; }
+        public void Drive(double distace)
+        {
+            double distan = fuelQuantity - (distace * fuelConsumption);
+            if (distan > 0)
+            {
+                fuelQuantity -= distan * fuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            return $"Make {this.Make} Model: {this.Model} Year: { this.Year} Fuel: { this.FuelQuantity:F2}";
+        }
+    }
+}
+
---
+++ C# Learning/C# Advanced/Defining Classes/01. Car/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            //var tires = new Tire[4]
+            //{
+            //    new Tire(1,2.5),
+            //    new Tire(1,2.1),
+            //    new Tire(2,0.5),
+            //    new Tire(2,2.3)
+            //};
+            //var engine = new Engine(560, 6300);
+            //var car = new Car("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
+
+            string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<int, double> TireDictionary = new Dictionary<int, double>();
+
+            while (command[0] != "No more tires")
+            {
+                for (int i = 0; i < command.Length; i++)
+                {
+                    int tireee = int.Parse(command[i]);
+                    double press = double.Parse(command[i + 1]);
+                    TireDictionary.Add(tireee, press);
+                    i++;
+                }
+            }
+            command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
