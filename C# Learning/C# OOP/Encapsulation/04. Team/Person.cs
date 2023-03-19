cf8736d9a6c92fd9d33395c57d8f342c0206404b
Vladimir Beev <vladimir.beev89@gmail.com>
2023-03-19 22:09:02 +0200

    Add files via upload

---
+++ C# Learning/C# OOP/Encapsulation/04. Team/Person.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstname, string lastname, int age, decimal salary)
+        {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Age = age;
+            this.Salary = salary;
+        }
+        private string _firstname;
+        private string _lastname;
+        private int _age;
+        private decimal _salary;
+        public string Firstname
+        {
+            get
+            {
+                return _firstname;
+            }
+            private set
+            {
+
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                else
+                    _firstname = value;
+            }
+        }
+        public string Lastname
+        {
+            get
+            {
+                return _lastname;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                else
+                    _lastname = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                else
+                    _age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            private set
+            {
+                _salary = value;
+            }
+        }
+    }
+}
---
+++ C# Learning/C# OOP/Encapsulation/04. Team/Team.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string _name;
+        private readonly List<Person> _firstTeam;
+        private readonly List<Person> _reserveTeam;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            this._firstTeam = new List<Person>();
+            this._reserveTeam = new List<Person>();
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            private set
+            {
+                _name = value;
+            }
+        }
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get 
+            { 
+                return this._firstTeam.AsReadOnly(); 
+            }
+        }
+        public IReadOnlyCollection<Person> ReservedTeam
+        {
+            get
+            {
+                return this._reserveTeam.AsReadOnly();
+            }
+        }
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                this._firstTeam.Add(person);
+            }
+            else
+            {
+                this._reserveTeam.Add(person);
+            }
+
+        }
+        public override string ToString()
+        {
+            return $"First team has {this.FirstTeam.Count} players.\n" +
+                   $"Reserve team has {this.ReservedTeam.Count} players.";
+        }
+    }
+}
