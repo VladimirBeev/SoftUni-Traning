eaea3fbdd44df14e7cffd6f8839e66ffeb81b7af
Vladimir Beev <vladimir.beev89@gmail.com>
2023-03-19 22:15:25 +0200

    Add files via upload

---
+++ C# Learning/C# OOP/Interfaces and Abstraction/Cars/Seat.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        private string model;
+        private string color;
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+        public string Color
+        {
+            get
+            {
+                return this.color;
+            }
+            set
+            {
+                this.color = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Seat {this.Model}\n" +
+                    $"{Start()}\n" +
+                    $"{Stop()}";
+        }
+    }
+}
---
+++ C# Learning/C# OOP/Interfaces and Abstraction/Cars/Tesla.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        private string model;
+        private string color;
+        private int battery;
+        public Tesla(string model,string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public string Model 
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+        public string Color 
+        {
+            get
+            {
+                return this.color;
+            }
+            set
+            {
+                this.color = value;
+            }
+        }
+        public int Battery 
+        {
+            get
+            {
+                return this.battery;
+            }
+            set
+            {
+                this.battery = value;
+            }
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla {this.Model} with {this.Battery} Batteries\n" +
+                $"{Start()}\n" +
+                $"{Stop()}";
+        }
+    }
+}
