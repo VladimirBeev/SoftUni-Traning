9d39dd03762ff2460d2bf7c5af5eb4b149a90e5f
Vladimir Beev <vladimir.beev89@gmail.com>
2023-03-19 22:00:48 +0200

    Add files via upload

---
+++ C# Learning/C# OOP/Inheritance - Ex/Animals/Animal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        public Animal(string name,int age,string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+        private string _name;
+        private int _age;
+        private string _gender;
+        public abstract string ProduceSound();
+
+        
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(this.Name),"Invalid input!");
+                }
+                this._name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentNullException(nameof(this.Age), "Invalid input!");
+                }
+                this._age = value;
+            }
+        }
+        public string Gender
+        {
+            get
+            {
+                return _gender;
+            }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(this.Gender), "Invalid input!");
+                }
+                this._gender = value;
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.GetType().Name}")
+                .AppendLine($"{this.Name} {this.Age} {this.Gender}")
+                .AppendLine($"{this.ProduceSound()}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Learning/C# OOP/Inheritance - Ex/Animals/StartUp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<Animal> animals = new List<Animal>();
+
+            string command;
+            while((command = Console.ReadLine()) != "Beast!")
+            {
+                try
+                {
+                    string[] animalInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray(); ;
+                    string name = animalInfo[0];
+                    int age = int.Parse(animalInfo[1]);
+                    string gender = animalInfo[2];
+                    Animal animal = null;
+                    if (command == "Dog")
+                    {
+                        animal = new Dog(name, age, gender);
+                    }
+                    else if (command == "Frog")
+                    {
+                        animal = new Frog(name, age, gender);
+                    }
+                    else if (command == "Cat")
+                    {
+                        animal = new Cat(name, age, gender);
+                    }
+                    else if (command == "Kitten")
+                    {
+                        animal = new Kitten(name, age);
+                    }
+                    else if (command == "Tomcat")
+                    {
+                        animal = new Tomcat(name, age);
+                    }
+                    else
+                        throw new Exception("Invalid input!");
+
+                    animals.Add(animal);
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Invalid input!");
+                }
+                
+            }
+            foreach (Animal ani in animals)
+            {
+                Console.WriteLine(ani);
+            }
+        }
+    }
+}
