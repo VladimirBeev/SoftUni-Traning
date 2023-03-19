68fd122f3a17c533e7cbe190060a6100ae386a03
Vladimir Beev <vladimir.beev89@gmail.com>
2023-03-19 22:04:23 +0200

    Add files via upload

---
+++ C# Learning/C# OOP/Inheritance - Ex/Restaurant/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Product
+    {
+        public Product(string name , decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        private string _name;
+        private decimal _price;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+    }
+}
