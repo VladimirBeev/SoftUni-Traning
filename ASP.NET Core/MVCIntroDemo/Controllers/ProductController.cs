4bec393c440b043f43e714e24fbd1ed59748cd71
Vladimir Beev <vladimir.beev89@gmail.com>
2023-06-07 01:11:22 +0300

    Add Download Products As Text File

--- ASP.NET Core/MVCIntroDemo/Controllers/ProductController.cs
+++ ASP.NET Core/MVCIntroDemo/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 using MVCIntroDemo.Models.Product;
 using Newtonsoft.Json;
+using System.Text;
 using System.Text.Json;
 
 namespace MVCIntroDemo.Controllers
@@ -53,6 +55,28 @@ namespace MVCIntroDemo.Controllers
 				WriteIndented = true
 			};
 			return Json(_products, options);
+		}
+		public IActionResult AllAsText()
+		{
+			var text = string.Empty;
+			foreach (var item in _products)
+			{
+				text += $"Product {item.Id}: {item.Name} - {item.Price} lv.";
+				text += "\r\n";
+			}
+			return Content(text);
+		}
+		public IActionResult AllAsTextFile()
+		{
+			StringBuilder sb = new StringBuilder();
+			foreach (var item in _products)
+			{
+				sb.AppendLine($"Product {item.Id}: {item.Name} - {item.Price:f2} lv.");
+			}
+
+			Response.Headers.Add(HeaderNames.ContentDisposition, @"attachment;filename=products.txt");
+
+			return File(Encoding.UTF8.GetBytes(sb.ToString().TrimEnd()), "text/plain");
 		}
 	}
 }
