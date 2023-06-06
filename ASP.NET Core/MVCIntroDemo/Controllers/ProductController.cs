e134a2ba5527b553c8828821479adf4b448933f4
Vladimir Beev <vladimir.beev89@gmail.com>
2023-06-07 00:44:53 +0300

    Add Products Controller and View

---
+++ ASP.NET Core/MVCIntroDemo/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCIntroDemo.Models.Product;
+
+namespace MVCIntroDemo.Controllers
+{
+	public class ProductController : Controller
+	{
+		private IEnumerable<ProductViewModel> _products
+			= new List<ProductViewModel>()
+			{
+				new ProductViewModel()
+				{
+					Id= 1,
+					Name = "Cheese",
+					Price = 7.00
+				},
+				new ProductViewModel()
+				{
+					Id= 2,
+					Name = "Ham",
+					Price = 5.50
+				},
+				new ProductViewModel()
+				{
+					Id= 3,
+					Name = "Bread",
+					Price = 1.50
+				}
+			};
+		public IActionResult All()
+		{
+			return View(_products);
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
