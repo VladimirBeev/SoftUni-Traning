a4dd4a9ddb5fa57b1267ed8c5126aa769d91ec95
Vladimir Beev <vladimir.beev89@gmail.com>
2023-06-08 03:43:13 +0300

    Add TextSplitter App and Controller

---
+++ ASP.NET Core/TextSplitterApp/TextSplitterApp/Models/TextSplitterModel/TextViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TextSplitterApp.Models.TextSplitterModel
+{
+	public class TextViewModel
+	{
+		public string Text { get; set; } = null!;
+
+		public string SplitTex { get; set; } = null!;
+	}
+}
